8b0f63b26ed1e654e21a465c5b8a85be24781d1c
zhjwang <695331215@qq.com>
2020-11-14 23:34:04 +0800

    回文（palindrome）字符串判断

---
+++ src/回文字符串/java/PalinDrome.java
@@ -0,0 +1,37 @@
+package 回文字符串.java;
+
+public class PalinDrome {
+    private boolean isNumber(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+    }
+
+    private boolean isEqualIgnoreCae(char a, char b) {
+        if (a >= 'A' && a <= 'Z') a += 32; //全部转换成小写
+        if (b >= 'A' && b <= 'Z') b += 32;
+        return a == b;
+    }
+
+    public int isPalindrome(String s) {
+        if (s == null || s.length() == 0)
+            return 0;
+        int i = 0, j = s.length() - 1; //两个变量，分别指向头尾
+        while (i <= j){
+            if(!isNumber(s.charAt(i)))
+                i++;
+            if(!isNumber(s.charAt(j)))
+                j--;
+            if(!isEqualIgnoreCae(s.charAt(i),s.charAt(j)))
+                return 1;
+            i++;
+            j--;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        String s = "abc3cb3";
+        PalinDrome palinDrome = new PalinDrome();
+        int result = palinDrome.isPalindrome(s);
+        System.out.println(result);
+    }
+}
