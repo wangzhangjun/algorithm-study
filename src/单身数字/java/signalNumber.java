fcc30eda94149c65b39b1f2c7025b28b63e18fe2
zhjwang <695331215@qq.com>
2021-02-06 22:17:24 +0800

    求单身数字

--- src/单身数字/java/signalNumber.java
+++ src/单身数字/java/signalNumber.java
@@ -5,11 +5,15 @@ import java.util.Set;
 
 /*
 * 什么是单身数字：比如 : 5,7,5,6,6
-* 除了7其他都是成对出现的。所以7就是单身数字
+* 除了7其他都是成对出现的。所以7就是单身数字。
 *
 * */
 public class signalNumber {
 
+    /*
+    * 方法1：使用set的唯一性，已经存在的，一定不会再set中再次出现。
+    * 把set中的元素乘以2，然后减去原有的元素的和。就是我们要找出的。
+    * */
     public  int signaleNumberWithSet(int nums[]){
         Set<Integer> set = new HashSet<>();
         int sum = 0, signalSum = 0;
